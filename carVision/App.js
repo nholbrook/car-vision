3beaf89f7c202506cad6dd436fd66e09835af7fe
Nick Holbrook <ndh175@gmail.com>
2019-02-02 18:18:21 -0500

    Added camera funcality

--- carVision/App.js
+++ carVision/App.js
@@ -8,24 +8,64 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { Platform, Text, TextInput, View, Button, StatusBar, StyleSheet } from 'react-native';
+import { NodeCameraView } from 'react-native-nodemediaclient';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = { 'flashenable': false };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <View style={{ flex: 1, backgroundColor: '#333' }}>
+              <StatusBar
+                barStyle="light-content"
+                backgroundColor="#6a51ae"
+              />
+              <NodeCameraView
+                style={{ flex: 1 }}
+                ref={(vb) => { this.vb = vb }}
+                outputUrl={"testurl.com"}
+                camera={{ cameraId: 0, cameraFrontMirror: true }}
+                audio={{ bitrate: 32000, profile: 1, samplerate: 44100 }}
+                video={{ preset: 1, bitrate: 500000, profile: 1, fps: 15, videoFrontMirror: false }}
+                smoothSkinLevel={3}
+                autopreview={true}
+              />
+              <ActionButton
+                buttonColor="#1abc9c"
+                offsetY={24}
+                offsetX={16}
+                size={32}
+                hideShadow={true}
+                verticalOrientation='down'
+
+              >
+                <ActionButton.Item buttonColor='#9b59b6' title="Reverse Camera" onPress={() => {
+                  this.vb.switchCamera();
+                  this.state.flashenable = false;
+                }}>
+                  <Icon name="ios-reverse-camera-outline" style={styles.actionButtonIcon} />
+                </ActionButton.Item>
+                <ActionButton.Item buttonColor='#3498db' title="Switch Flashlight" onPress={() => {
+                  this.state.flashenable = !this.state.flashenable;
+                  this.vb.flashEnable(this.state.flashenable);
+                }}>
+                  <Icon name="ios-bulb-outline" style={styles.actionButtonIcon} />
+                </ActionButton.Item>
+                <ActionButton.Item buttonColor='#e6ce28' title="Publish" onPress={() => { this.vb.start() }}>
+                  <Icon name="ios-paper-plane-outline" style={styles.actionButtonIcon} />
+                </ActionButton.Item>
+                <ActionButton.Item buttonColor='#e74c3c' title="Close" onPress={() => { this.props.navigation.goBack() }}>
+                  <Icon name="ios-power-outline" style={styles.actionButtonIcon} />
+                </ActionButton.Item>
+              </ActionButton>
+            </View >
     );
   }
 }
