fe02a087cc690915e5265390738d0a0b624e475b
Nick Holbrook <ndh175@gmail.com>
2019-02-02 12:03:59 -0500

    Initial commit

---
+++ car-vision/car-visionUITests/car_visionUITests.swift
@@ -0,0 +1,34 @@
+//
+//  car_visionUITests.swift
+//  car-visionUITests
+//
+//  Created by Nick Holbrook on 2/2/19.
+//  Copyright © 2019 Nick Holbrook. All rights reserved.
+//
+
+import XCTest
+
+class car_visionUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
